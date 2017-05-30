186d1c79fded1719556c9fa88be6991c333e5750
hyj8832 <hyj8832@naver.com>
2017-05-30 20:24:53 +0900

    #2 Assign View Refence

--- app/src/main/java/com/example/admin/simplediary/MainActivity.java
+++ app/src/main/java/com/example/admin/simplediary/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.admin.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker date;
+    EditText edit;
+    Button but; //날짜가 동일하면 수정하기라고 버튼이 뜨게
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        date = (DatePicker) findViewById (R.id.date_pick);
+        edit = (EditText) findViewById (R.id.edit);
+        but = (Button) findViewById (R.id.but);
     }
 }
